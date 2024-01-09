af5b9c11c424141b0b8334882c145c6a8268ad1c
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-01-09 21:29:41 +0530

    Time: 6 ms (52.69%), Space: 58.1 MB (33.60%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -3,20 +3,42 @@ class Solution {
         //completed by myself
          //there is number from [1 N] so blindly apply cyclic sort
         //applying cyclic sort
-        int i = 0;
-        int dup = 0;
-        while (i < nums.length) {
-            int correct_index = nums[i]-1;
-            if (nums[i] < nums.length && nums[i] != nums[correct_index]) {
-                swap ( nums, i, correct_index );
-            } else {
-                dup = nums[i];
-                i++;
+//         int i = 0;
+//         int dup = 0;
+//         while (i < nums.length) {
+//             int correct_index = nums[i]-1;
+//             if (nums[i] < nums.length && nums[i] != nums[correct_index]) {
+//                 swap ( nums, i, correct_index );
+//             } else {
+//                 dup = nums[i]; //thoughts was mine and leetcode accepted it wow
+//                 i++;
 
+//             }
+//         }
+//         return dup;
+
+    //kunal
+        int i =0;
+        while(i<nums.length){
+            if(nums[i] != i+1){
+                int correct_index = nums[i]-1;
+                if(nums[i] != nums[correct_index]){
+                    swap(nums , i , correct_index);
+                }
+                else{
+                    return nums[i];
+                }
+                
+            }
+            else{
+                i++;
             }
         }
-        return dup;
+        return -1;
     }
+    
+    
+    
     //swapping the value of the array
     public static void swap(int[] nums, int first, int second) {
         int temp = nums[first];
