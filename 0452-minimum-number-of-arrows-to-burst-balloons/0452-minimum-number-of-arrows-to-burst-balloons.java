d3ef7d38f1e21ab342271bbe84dec16e481c5f48
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-03-18 09:50:53 +0530

    Time: 58 ms (28.51%), Space: 69.4 MB (22.08%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        int n = points.length;
+        // Arrays.sort(points);
+         Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int count = 1;
+
+        int[] prev = points[0]; //prev_pointer
+        for (int i = 1; i < n; i++) {
+            int currentstartpoint = points[i][0];
+            int currentendpoint = points[i][1];
+
+            if (currentstartpoint > prev[1]) {
+                //no overlapping then we need to shoot one more needle to burst the balloon and making current is previous
+                count++;
+                prev = points[i];
+//                 prev[0] = currentstartpoint;
+//                 prev[1] = currentendpoint;
+            } else {
+                //pakka overlap ho rha hai
+                //it overlap occurs then we will want more balloons to overlap
+                prev[0] = Math.max(currentstartpoint, prev[0]);
+                prev[1] = Math.min(currentendpoint, prev[1]);
+            }
+        }
+        return count;
+    }
+}
