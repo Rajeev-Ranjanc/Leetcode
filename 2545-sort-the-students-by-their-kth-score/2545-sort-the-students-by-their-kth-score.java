156371f2f5d87fdc843b68c9be2ef1aa3e642f87
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-03-23 10:03:55 +0530

    Time: 2 ms (94.89%), Space: 52.5 MB (18.56%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
@@ -1,5 +1,9 @@
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
+        /*
+            we just need to need to wtrite the code of comparator using comparator this can
+            be done easily
+        */
          Arrays.sort(score, (int[] a, int[] b) -> Integer.compare(b[k], a[k]));
         return score;
     }
