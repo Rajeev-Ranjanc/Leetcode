c400fa5c52ad20382f200ba1032a16dcd7add1b3
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-03-30 02:09:39 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (10.09%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -12,6 +12,10 @@ class Solution {
             this question is asking we just need to remove the link which is connected to the
             next node of this and the node which is given is the previous node of that node which
             is to be remove
+            
+            Same question is on gfg naing delete withour head pointer in which we just need to delete the
+            present node data from the next node or we an say that we just need to copy the data 
+            of its net node to the previous node
         */
         node.val = node.next.val;
         node.next = node.next.next;
