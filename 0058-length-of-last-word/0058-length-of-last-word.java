34e3239f929dacf70de52aa15ee2b9cac828523d
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-01 21:11:00 +0530

    Time: 2 ms (13.54%), Space: 41.6 MB (45.57%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,5 +1,9 @@
 class Solution {
     public int lengthOfLastWord(String s) {
+/*
+        this question can also say that find the longest comman prefix in this problem I am shifting right           elements of both left and right
+        until it becomes equal later we can shift that no of left shift which i have put right shift any            number eft or right because both are equals now
+ */
         s = s.trim();
         int count = 0;
         for (int i = 0; i < s.length(); i++) {
