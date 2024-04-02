9a00dc4d9c0d96e172d2f25e4a4cf3e932913e31
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-02 16:13:30 +0530

    Time: 37 ms (22.42%), Space: 44.8 MB (74.91%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -1,5 +1,11 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
+/*
+    I have approached this problem using Hashmaps I have put all the even element of the array in the Map and     count the number
+    of frequency each & every element, and we find the maximum frequencies among all the numbers and then         iterate over the keyset
+    of the hashmaps and if we find the equal no of highest frequent elements we return that key. One more           thing I took Treemap so
+    if we find the same frequent element twice then i can return the smaller one and I submitted them
+*/
         Map<Integer, Integer> map = new TreeMap<>();
         for (int num : nums) {
             if ((num % 2) == 0) {
