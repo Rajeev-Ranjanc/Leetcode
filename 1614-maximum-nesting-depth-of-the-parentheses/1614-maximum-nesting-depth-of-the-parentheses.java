e29373c91ac1382f256ab282b72479e72871c453
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-05 01:56:30 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (12.80%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -1,8 +1,18 @@
 class Solution {
     public int maxDepth(String s) {
-         int count = 0;
+        
+       /*
+        this question is saying find the depth of the parenthesis
+        this can be done using o(n)n extra space using stack data structure and
+        otger way is by not using any data structure in this approach simply count the opening 
+        parenthesis and
+        if the string contain closing one then decrease the count by 1 and always store maximum 
+        value before decreasing the count and finally return the maximum value;
+         */
+        
+        int count = 0;
         int result = 0;
-        Stack<Character> st = new Stack<>();
+        
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
                 count++;
