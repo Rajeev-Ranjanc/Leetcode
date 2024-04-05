9b3e36a36ad8ff199820ff40de2288903f661a39
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-05 23:01:19 +0530

    Time: 7 ms (26.93%), Space: 44.7 MB (35.27%) - LeetHub

--- 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.java
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isAcronym(List<String> words, String s) {
-        //just declare a String type v
+        //just declare a String type variable then add the first element of every words into them
+        // and check is that string is equal to the s string
          String ans="";
         for (String word : words) {
             ans += word.charAt(0);
