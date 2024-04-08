03f8137b6916a42bfb451547e09baa5d0489df65
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-09 00:36:31 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (97.49%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        //simulation problem
+        //take stack ans queue and simulate as given in the question
+        //simulation problem
+        //take stack ans queue and simulate as given in the question
+        int n = students.length;
+        int[] arr = new int[2];
+        //arr[0] = count of student who like 0 sandwich
+        //arr[1] = count of student who like 1 sandwich
+        for (int num : students) {
+            arr[num]++;
+        }
+
+        //Now sandwiches party
+        for (int i = 0; i < n; i++) {
+            int sand = sandwiches[i];
+
+            if (arr[sand]== 0) {
+                return n - i;
+            }
+            arr[sand]--;
+        }
+        return 0;
+
+    }
+}
