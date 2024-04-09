aadaa93ed356e5fd29c7a6e140ce679023985576
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-10 01:20:58 +0530

    Time: 0 ms (100.00%), Space: 57.3 MB (42.16%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,6 +1,10 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        
+        /*
+    Although this is not optimized one for optimization we can do this is 100 optimized one in this
+    First we reverse the array from 0 to n-1-k (first elements which does not contain kth item) then second time
+    reversed the array elements from n-k(from kth) to length of the array and then again reverse the complete array
+     */
         int n = nums.length;
         k = k % n;
         reverse(nums, 0, n - 1 - k);
