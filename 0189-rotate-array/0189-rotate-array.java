bfa6b12f810ec45358004d9d00ff535ad6b255f7
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-10 01:18:01 +0530

    Time: 0 ms (100.00%), Space: 57.5 MB (28.84%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,30 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-/*
-        In this problem I have approached this first I reverse entire array, and then I have revered till           the Kth item and after that 
-        again I reversed the rest array from k+1 to length of the array and finally simply returns
-*/
+        
         int n = nums.length;
         k = k % n;
+        reverse(nums, 0, n - 1 - k);
+        reverse(nums, n - k, n-1);
+        reverse(nums, 0, n-1);
         
-        //reversing entire array
-        for (int i = 0; i < n / 2; i++) {
-            swap(nums, i, n - 1 - i);
-        }
-        
-        //reversing upto kth element
-        for (int i = 0; i < k / 2; i++) {
-            swap(nums, i, k - i - 1);
-        }
-        //now again reversing rest half of the array
-        for (int i = k; i < (n + k) / 2; i++) {
-            swap(nums, i, (n + k - 1 - i));
-            // System.out.println();
-//            System.out.println(i + " " + (n + k - 1 - i));
-        }
-
     }
-
+    
+    public void reverse(int[] nums, int start , int end) {
+        while (start <= end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
     public static void swap(int[] nums, int first, int second) {
         int temp = nums[first];
         nums[first] = nums[second];
