308bd54ec50f346a5ab061dac1499f3f898fb8a1
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-10 00:59:21 +0530

    Time: 2 ms (32.06%), Space: 57.3 MB (42.14%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,14 +1,22 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+/*
+        In this problem I have approached this first I reverse entire array, and then I have revered till           the Kth item and after that 
+        again I reversed the rest array from k+1 to length of the array and finally simply returns
+*/
         int n = nums.length;
         k = k % n;
+        
+        //reversing entire array
         for (int i = 0; i < n / 2; i++) {
             swap(nums, i, n - 1 - i);
         }
-
+        
+        //reversing upto kth element
         for (int i = 0; i < k / 2; i++) {
             swap(nums, i, k - i - 1);
         }
+        //now again reversing rest half of the array
         for (int i = k; i < (n + k) / 2; i++) {
             swap(nums, i, (n + k - 1 - i));
             // System.out.println();
