6b35dd159224c38e620af43a1147af83e44f5126
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-18 11:18:08 +0530

    Time: 2 ms (21.73%), Space: 45.5 MB (38.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,7 +1,7 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        //its getting error but in this im trying to store the index where ith and jth are 0 and if it is so we             store into an list that index and then we put 0 at that index by checking that is that index contains              in the list
-         Set<Integer> zeroRows = new HashSet<>();
+        //its getting error but in this im trying to store the index where ith and jth are 0 and if it is so we           store into an list that index and then we put 0 at that index by checking that is that index                    contains in the list to solve this i have simply iterate over the matrix and checked where element is zero        and if we counter any zero then we store the i and j th value in a set because when we will get .contains       then set will return the yes or no in O(1) tc and later again traverse the array and checked that if that row or column contains in set then set that element to zero thats it
+        Set<Integer> zeroRows = new HashSet<>();
         Set<Integer> zeroCols = new HashSet<>();
 
         for (int i = 0; i < matrix.length; i++) {
