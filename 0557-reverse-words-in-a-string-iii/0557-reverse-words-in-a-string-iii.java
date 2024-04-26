9323daa119da25e255ff4607e5462722f6de1bb0
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-26 12:35:09 +0530

    Time: 6 ms (61.65%), Space: 45 MB (81.83%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,21 +1,51 @@
 class Solution {
     public String reverseWords(String s) {
-       String ans = "";
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch != ' ') {
-                stack.push(ch);
+       // String ans = "";
+       //  Stack<Character> stack = new Stack<>();
+       //  for (int i = 0; i < s.length(); i++) {
+       //      char ch = s.charAt(i);
+       //      if (ch != ' ') {
+       //          stack.push(ch);
+       //      } else {
+       //          while (!stack.isEmpty()) {
+       //              ans += stack.pop();
+       //          }
+       //          ans += " ";
+       //      }
+       //  }
+       //  while (!stack.isEmpty()) {
+       //      ans += stack.pop();
+       //  }
+       //  return ans;
+        StringBuilder sb = new StringBuilder(s);
+       
+        int i = 0;
+        int j = 0;
+        int n = s.length();
+        while(j<n){
+            if (sb.charAt(j) != ' ') {
+                j++;
             } else {
-                while (!stack.isEmpty()) {
-                    ans += stack.pop();
-                }
-                ans += " ";
+                reverse(sb, i, j - 1);
+                i = j + 1;
+                j = i;
             }
         }
-        while (!stack.isEmpty()) {
-            ans += stack.pop();
+         reverse(sb, i, j - 1);
+        return sb.toString();
+        
+    }
+        
+    
+     public static void reverse(StringBuilder sb, int i, int j) {
+        //now reverse from i to j how?? by swapping!
+        while (i <= j) {
+            char ch = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(j));
+            sb.setCharAt(j, ch);
+            i++;
+            j--;
         }
-        return ans;
+
     }
 }
