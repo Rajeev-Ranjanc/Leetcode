92452518b20d2278b39304f693895e37e40d6de4
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-27 18:59:34 +0530

    Time: 86 ms (9.98%), Space: 55.4 MB (76.21%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,5 +1,14 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+       
+/*
+    I'm approaching this problem using hashmaps in that we first we simply declare a hashmaps and
+    i that I'm storing numbers of array as key and their indexes as the value if any elements of I
+    does not appear into the array only then I'm inserting that element into that map otherwise I'm
+    checking the previous Index and present Indexes differences is less than or equal to k or not if
+    not then again I'm updating the previous indexes for the future check and done
+ */
+
          Map<Integer, Integer> map = new TreeMap<>();
         for (int i = 0; i < nums.length; i++) {
             if(!map.containsKey(nums[i])){
