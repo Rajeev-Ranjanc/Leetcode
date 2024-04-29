45361ae56c565879b906b30771eb5ecbc150c7fa
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-29 12:27:31 +0530

    Time: 1754 ms (100.00%), Space: 41.1 MB (100.00%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.java
+++ 3133-minimum-array-end/3133-minimum-array-end.java
@@ -1,5 +1,9 @@
 class Solution {
     public long minEnd(int n, int x) {
+        /*
+        in this problem I'm running a loop n-1 times and declare a varible ans in that We store the x and                 evrytime first we increase the ans by 1 everytime and doing or operation with x and ans and finally
+        returned the ans
+        */
 
         long ans=x;
         for(int i=0;i<n-1;i++)
