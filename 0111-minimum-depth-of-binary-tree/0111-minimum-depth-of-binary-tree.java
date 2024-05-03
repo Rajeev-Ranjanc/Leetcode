9a0edb301c4e84c5a20a9a593607304d8a00d648
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-05-03 17:41:43 +0530

    Time: 4 ms (78.71%), Space: 62.9 MB (82.78%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -15,6 +15,19 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
+        /*
+       
+        Preety simple forward we just called a recursion for the left subtree which will return the
+        height of the left sub-tree and another recusrsion will return height of right subtree then we 
+        will simply return the minimum from both of them + 1 if and only if left and right both will not be 
+        equal to 0(Zero) and for every left node that recursion call will go left and right and will find null              so that leaf node will find min(0 , 0 ) + 1 to their
+        parent node and that node will also do same for their recursive call and 
+        if the any subtree will be equal to zero then we will return the maximum depth because if left node
+        will be null then that is not any subtree so we can't say that will be depth of that tree because
+        there is no node
+            
+
+        */
         int count = 0;
         if(root == null){
             return 0;
