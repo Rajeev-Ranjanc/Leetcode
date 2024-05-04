eebb101954fae93e2af32d4cd229468b1bd64524
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-05-04 12:09:13 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (91.84%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -17,6 +17,11 @@ class Solution {
    
     
     public List<Integer> preorderTraversal(TreeNode root) {
+        /*
+        This is the first way to do so in which we declare list inside the function we can do same after 
+        declaring the list outside the function and keep adding elements into the list and finally return 
+        the list
+        */
         List<Integer> list = new ArrayList<>();
         if(root == null){
            
