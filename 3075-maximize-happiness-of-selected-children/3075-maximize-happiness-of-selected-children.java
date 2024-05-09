2b5a9b46b7abdb0ba3ccd9a5b70c2048d236bf26
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-05-09 11:04:26 +0530

    Time: 34 ms (97.44%), Space: 62 MB (20.21%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -1,5 +1,13 @@
 class Solution {
-    public long maximumHappinessSum(int[] happiness, int k) {
+    public long maximumHappinessSum(int[] happiness, int k) {/*
+        Not pretty hard simple and forward as we have to store maximum k element from array but after every selection
+        other elements will decrease by one and finally produce ans to implement this I just took a variable of return
+        type and sort the array and start traversing from behind of the array and everytime once I consider any number
+        as answer+ then decreases the index sp first i can select last biggest and then second last and then third last
+        and so on and every time before adding any happiness element into the answer we subtract a value which is count
+        which decreases by one on every selection of any element from the array and finally returned the ans don't forget
+        to take ans of long type I got an error when I took the ans as of integer type so take answer as of long type
+ */
             
         long ans = 0;
         Arrays.sort(happiness);
