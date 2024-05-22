1340e7b7ec0d6e6db9bb3c2b0b2862763e65e0aa
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-05-23 01:44:39 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (42.86%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,12 +10,21 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+/*
+            To solve this i have defined two head nodes one is for the odd numbers and other is for the even        numbers  and declare two odd and even variables to traverse the linked list. one itt var is also declared       to count the indexes like odd or even and if itt is even then added that node into even numbers otherwise       into odd
+    inked list and till we came up creating two linked list, and finally I linked even linked list with odd         list,  and we connect head with next part of even head(evenDummy)
+
+     */
+        ListNode oddDummy = new ListNode(-1);
         
-         ListNode oddDummy = new ListNode(-1);
         ListNode evenDummy = new ListNode(-1);
+        
         ListNode odd = oddDummy;
+        
         ListNode even = evenDummy;
+        
         int itt = 0;
+        
         ListNode temp = head;
 
         while (temp != null) {
@@ -23,16 +32,19 @@ class Solution {
             if (itt % 2 == 0) {
 
                 even.next = temp;
+                
                 even = even.next;
 
             } else {
 
                 odd.next = temp;
+                
                 odd = odd.next;
 
             }
 
             temp = temp.next;
+            
             itt++;
 
         }
@@ -47,7 +59,9 @@ class Solution {
         //for even numver before
 
         odd.next = null;
+        
         even.next = oddDummy.next;
+        
         head = evenDummy.next;
         
         return head;
