7e88e77ddbfde1c1e6f621e03a4a8ea4dc140c1d
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-05-31 10:56:26 +0530

    Time: 2 ms (25.29%), Space: 41.4 MB (52.72%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        
+        /*
+
+    https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+    Meta Asked
+    As mentioned in the question that string contains only parenthesis so, I just 
+    need to check if it is closing parenthesis then check the top of stack is it 
+    open if stack is not open if yes them simply pop the brackets else put the ch
+    into the stack.else part of the opening bracket also put the character into stack.
+
+ */
+        
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == ')') {
+
+                if (!stack.isEmpty() && stack.peek() == '(') {
+
+                    stack.pop();
+
+                } else {
+
+                    stack.push(ch);
+
+                }
+
+            } else {
+
+                stack.push(ch);
+
+            }
+
+        }
+
+        return stack.size();
+        
+    }
+}
