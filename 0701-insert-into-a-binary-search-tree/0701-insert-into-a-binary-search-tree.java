9caef159f88b83df8898f028eb063568f9e2bafd
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-06-07 12:03:02 +0530

    Time: 0 ms (100.00%), Space: 45.4 MB (12.35%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -15,6 +15,18 @@
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
+ 
+/*
+        In this question I'm inserting the nodes at the leaf node always to reach the leaf node
+        I have created a copy of root node(temp) and checked if temp.val is less than val if yes
+        then again check if their right is not null then made temp = temp.right else it's next is 
+        null then I can insert myNode here then inserted temp.right = newNode and break the loop
+        else(outer if) means temp.val is not less than the given val then inside that again made a 
+        check is their left is null if not then simply temp = temp.left else it means their left is
+        null it means I'm at leaf node and I can insert my node so inserted and break loop and return
+        the root
+        
+*/
         TreeNode newNode = new TreeNode(val);
         if (root == null) {
             root = newNode;
