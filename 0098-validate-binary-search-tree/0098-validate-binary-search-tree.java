8e37b16832b73c5b46887c6671deb7d69fb98352
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-06-07 16:27:40 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (66.72%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,7 +15,15 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        
+/*
+        https://leetcode.com/problems/validate-binary-search-tree/
+        Pretty Interesting question I can do it by providing a range to our helper function.
+        First I provide the root and Long MIN value for min range LONG max for max range and
+        in helper function I have returned true if root is null and check if root.val is less than 
+        then min val or root.val is greater than the max val then return false and for the left
+        recursive call min will be min but max will be root.val and for right recursive call min will be 
+        root.val and max will be long max that's it
+*/
         return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
         
     }
