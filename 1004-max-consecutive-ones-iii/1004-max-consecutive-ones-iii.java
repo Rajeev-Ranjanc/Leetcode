05a488427738f9162be39c9e1865c968f531ddcc
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-06-11 08:46:25 +0530

    Time: 4 ms (24.74%), Space: 46.9 MB (48.17%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,6 +1,7 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         /*
+        
         This is better according to striver because  TC : O(2N) & SC : O(N)
             
         Simply ran a while loop till j is less than the nums.length & check if nums[i] is equal to the 0 then
@@ -8,7 +9,7 @@ class Solution {
         than the k increased i and decreased zeros if nums[i] is 0 and then store maximum val everytime into ans
         variable and increased the jth
         
-        */
+       
         int i = 0;
         
         int j = 0;
@@ -36,6 +37,40 @@ class Solution {
             
             ans = Math.max(ans, j - i + 1);
 
+            j++;
+        }
+        
+        return ans;
+        
+    */
+        /*
+            Optimal solution SC : O(1) && TC : O(N)
+            There is nothing big changes only change is that I'm updating the max only when no of zero is less
+            or equal than the k
+         */
+
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+        int zero = 0;
+
+        while (j < nums.length) {
+
+            if (nums[j] == 0) {
+                zero++;
+            }
+
+            if (zero > k) {
+
+                if (nums[i] == 0) {
+                    zero--;
+                }
+
+                i++;
+            }
+            
+            ans = Math.max(ans, j - i + 1);
+            
             j++;
         }
         
